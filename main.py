c1103a547b1131f8f84979955f9138c6d1a5800a
Draw <74398819+AndrewDTR@users.noreply.github.com>
2022-07-09 23:17:42 -0400

    Added handling of incorrect inputs

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 
 first_num = input("Enter your first number: ")
 
+while not first_num.isnumeric():
+    first_num = input("That's not a number. Try again: ")
+
 operator = input("Enter the operation that you'd like to perform: ")
 
+while not operator in ["*", "-", "+", "/"]:
+    operator = input("Sorry, I don't recognize that operator. Try again: ")
+
 second_num = input("Enter the second number: ")
+
+while not second_num.isnumeric():
+    second_num = input("That's not a number. Try again: ")
+
