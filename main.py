0c8b7aaf5817f8214e870ae2db6524795d8651ea
Draw <74398819+AndrewDTR@users.noreply.github.com>
2022-07-09 23:12:12 -0400

    Added initial input boxes

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # A calculator will go here.
+
+first_num = input("Enter your first number: ")
+
+operator = input("Enter the operation that you'd like to perform: ")
+
+second_num = input("Enter the second number: ")
